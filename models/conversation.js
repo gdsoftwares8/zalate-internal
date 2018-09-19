c33bf18fb8924b9a85ed9d968c797c0f5e8b803a
Vishwas <vishwasvaishnav22@gmail.com>
2018-09-20 00:41:30 +0530

    Zalate Backend (V 0.0.2)
    #message schema update

--- models/conversation.js
+++ models/conversation.js
@@ -7,11 +7,24 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
+const ChatType = {
+    Personal: 0,
+    Group: 1,
+    BroadcastGroup: 2,
+    Anonymous: 3,
+    Incognito: 4,
+    Support:5
+};
+
 var conversation = new Schema({
-    name: {type: String, required: false},
+    convoName: {type: String, required: false},
+    adminID: {type: String, required: true, index: true,ref: 'User'},//who created convo ie. The Admin
     initDate: {type: Number, required: true},
-    groupNamed: {type: Boolean, 'default': true, required: false},
-    users: [{type: ObjectId, required: true, ref: 'User'}] //participants
+    lastDate: {type: Number, required: true},
+    chatType: {type: Number, required: true},
+    users: [{type: String, required: true, ref: 'User'}]
 });
 
+
 exports.Conversation = mongoose.model('Conversation', conversation);
+exports.ChatType = ChatType;
--- models/message.js
+++ models/message.js
@@ -6,10 +6,34 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
-var message =  new Schema({
-    from:{ type: ObjectId, required: true,ref: 'User' },
-    conversationID:{ type: ObjectId, required: true,ref: 'Conversation'  },
-    date:{type:Number,required:true,index: true},
-    message:{type:String,required:false,'default':""},
+var message = new Schema({
+    from: {type: String, required: true, ref: 'User'},
+    convoID: {type: ObjectId, required: true, ref: 'Conversation'},
+    mediaUrl: {type: String, required: false, 'default': ""},
+    date: {type: Number, required: true, index: true},
+    type: {type: Number, required: true},
+    message: {type: String, required: false, 'default': ""},
+    status: {type: Number, required: false},
+    readers: [{type: String, required: true, ref: 'User'}]
 });
 exports.Message = mongoose.model('Message', message);
+exports.MessageType = {
+    TEXT: 0,
+    PICTURE: 1,
+    VOICE: 2,
+    LOCATION: 3,
+    EVENT: 4,
+    ERROR: 5,
+    LINK: 6,
+    DOC: 7,
+    FILE: 8
+};
+
+exports.MessageStatus = {
+    SEND_STATUS_SENDING: 0,
+    SEND_STATUS_SUCCESS: 1,
+    SEND_STATUS_FAIL: 2,
+    SEND_STATUS_DELIVERED: 3,
+    SEND_STATUS_VIEWED: 4,
+    SEND_STATUS_VIEWED_ALL: 5
+};
